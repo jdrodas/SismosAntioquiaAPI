8458008a058e80d3b362981dc6b29758d1566267
jdrodas <jdrodas@hotmail.com>
2022-05-04 21:04:19 -0500

    Agregados los JsonPropertyName a la clase Sismo

--- SismosAntioquiaAPI/SismosAntioquiaAPI/Models/Sismo.cs
+++ SismosAntioquiaAPI/SismosAntioquiaAPI/Models/Sismo.cs
@@ -8,25 +8,32 @@ namespace SismosAntioquiaAPI.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
+        [JsonPropertyName("id")]
         public string? Id { get; set; }
 
         [BsonElement("region")]
+        [JsonPropertyName("region")]
         public string Region { get; set; } = null!;
 
         [BsonElement("fechaHora")]
+        [JsonPropertyName("fechaHora")]
         [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
         public DateTime FechaHora { get; set; }
 
         [BsonElement("magnitud")]
+        [JsonPropertyName("magnitud")]
         public double Magnitud { get; set; }
 
         [BsonElement("profundidad")]
+        [JsonPropertyName("profundidad")]
         public double Profundidad { get; set; }
 
         [BsonElement("longitud")]
+        [JsonPropertyName("longitud")]
         public double Longitud { get; set; }
 
         [BsonElement("latitud")]
+        [JsonPropertyName("latitud")]
         public double Latitud { get; set; }
     }
 }
